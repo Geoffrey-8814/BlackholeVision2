12736eb9ca79ab847cc8343fc8238287c9bc7579
Geoffrey-8814 <Geoffreyxiao8814@gmail.com>
2025-03-29 16:03:44 +0800

    finished Training Model

--- test.py
+++ test.py
@@ -4,21 +4,23 @@ import cv2
 # Load a COCO-pretrained YOLO11n model
 
 
-model = YOLO("runs\\detect\\train5\\weights\\last.pt")
+model = YOLO("runs\\detect\\train6\\weights\\last.pt")
 
 model.to('cuda')
 
 
 
 # initialize camera
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc(*'MJPG'))#without this the cap will run at 5 or 10fps for arducam
 cap.set(cv2.CAP_PROP_SETTINGS, 0)#reset to default
 cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 3) # auto mode
 cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1) # manual mode
+
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)# set frame height
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)# set frame width
-
+cap.set(cv2.CAP_PROP_EXPOSURE, 4)# Set exposure value
+cap.set(cv2.CAP_PROP_GAIN, 0)# Set sensor gain
 while True:
     ret, frame = cap.read()
     if not ret:
